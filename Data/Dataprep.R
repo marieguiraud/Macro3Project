b79ec01232fefbc91e8afa0a82e122a51d08b28e
julesoudot <jlsoudot@gmail.com>
2026-05-15 01:36:57 +0200

    Création de la base avec presque tout

--- Data/Dataprep.R
+++ Data/Dataprep.R
@@ -1,5 +1,7 @@
 library(WDI)
 library(tidyverse)
+library(dplyr)
+library(readxl)
 
 # ── 1. Pull all WDI-available Chinn-Prasad variables ────────────────────────
 
@@ -10,12 +12,12 @@ wdi_raw <- WDI(
     "SP.POP.DPND.YG",      # RELDEPY (raw, needs normalization)
     "SP.POP.DPND.OL",      # RELDEPO (raw, needs normalization)
     "NY.GDP.MKTP.KD.ZG",   # YGRAVG + YGRSD (same series, both derived)
+    "TT.PRI.MRCH.XD.WD",    #TOTSD
     "PX.REX.REER",         # LREER (needs log)
     "NE.TRD.GNFS.ZS",      # OPEN
     "FM.LBL.BMNY.GD.ZS",   # FDEEP
-    "NY.GNS.ICTR.ZS",       # NSGDP
-    "TT.PRI.MRCH.XD.WD"
-  ),
+    "NY.GNS.ICTR.ZS"    # NSGDP
+    ),
   country = "all",
   start   = 1960,
   end     = 2023,
@@ -87,4 +89,100 @@ panel <- wdi_clean |>
     .groups = "drop"
   ) |>
   select(-period)
+
+#Introduction of the data set for KC2 and KC3 gathered in ka_open 
+setwd("C:/Users/jlsou/OneDrive/Documents/GitHub/Macro3Project/Data")
+ka_open<- read.csv("ka_open.csv")
+install.packages("countrycode")
+library(countrycode)
+
+#the goal is to transform the entire name of countries to iso3c.
+ka_open$iso3 <- countrycode(
+  ka_open$country_name,
+  origin = "country.name",
+  destination = "iso3c"
+)
+#some values have not succeed in to be transform because strange name like S? Tom for Sao Tom and Principe
+unique(ka_open$country_name[is.na(ka_open$iso3)])
+#we delete this countries: one is an island of Netherlands and the other Sao Tome and Principe
+
+ka_open <- subset(
+  ka_open,
+  !country_name %in% c(
+    "Netherlands Antilles",
+    "S? Tom and Principe"
+  )
+)
+
+#I delete the column country_name and I put iso3 in first column
+ka_open <- ka_open %>%
+  select(iso3, everything(), -country_name)
+
+#I clean for data from 71 to today and i compute a mean for period of 5 years
+ka_open_clean <- ka_open |>
+  filter(year >= 1971) |>
+  mutate(period = (year - 1971) %/% 5) |>
+  group_by(iso3, period) |>
+  summarise(
+    year    = min(year),          # label each period by its first year
+    ka_open = mean(ka_open,   na.rm = TRUE),
+    .groups = "drop"
+  ) |>
+  select(-period)
+
+# I merge ka_open_clean in panel 
+panel_2<- panel %>%
+  left_join(ka_open_clean, by = c("iso3", "year"))
+
+#introduction of the NFAGDP variable
+NFAGDP<-read_xlsx("NFAGDP.xlsx")
+
+#I keep only variable Country, year and Net IIP excl gold
+
+NFAGDP_clean <- NFAGDP %>%
+  select("Country", "Year", "net IIP excl gold / GDP domestic currency")
+
+#I transform Country from the letter name to iso3
+NFAGDP_clean$iso3 <- countrycode(
+  NFAGDP_clean$Country,
+  origin = "country.name",
+  destination = "iso3c"
+)
+
+#I see observations with a problem in their iso3
+unique(NFAGDP_clean$Country[is.na(NFAGDP_clean$iso3)])
+
+#I delete these information of the panel
+NFAGDP_clean_2 <- subset(
+  NFAGDP_clean,
+  !Country %in% c(
+    "Netherlands Antilles",
+    "Kosovo",
+    "ECCU",
+    "Euro Area",
+    "Micronesia"
+  ))
+
+#I delete the column Country and I pu the column iso3 in first and i rename the column Year in year
+NFAGDP_clean_2 <- NFAGDP_clean_2 %>%
+  select(iso3, everything(), -Country) %>% 
+  rename(year = Year) %>%
+  rename(NFAGDP = `net IIP excl gold / GDP domestic currency` )
+
+#I create the mean for each period of 5 years 
+NFAGDP_clean_2 <- NFAGDP_clean_2 |>
+  filter(year >= 1971) |>
+  mutate(period = (year - 1971) %/% 5) |>
+  group_by(iso3, period) |>
+  summarise(
+    year    = min(year),          # label each period by its first year
+    NFAGDP = mean(NFAGDP,   na.rm = TRUE),
+    .groups = "drop"
+  ) |>
+  select(-period)
+
+#je merge la base de donnée NFAGDP_clean_2 avec panel_2
+panel_3<- panel_2 %>%
+  left_join(NFAGDP_clean_2, by = c("iso3", "year"))
+
 
